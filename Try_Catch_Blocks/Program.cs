70e804a77b08402f6b2c28eaff99183f0fcabff3
Murat Utlu <utlumurat@gmail.com>
2021-07-15 12:27:44 +0300

    launch.json file edit and Try_catch_blocks added

---
+++ Try_Catch_Blocks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Try_Catch_Blocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message.ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("Completed!!");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+                
+            }
+            catch(ArgumentNullException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
